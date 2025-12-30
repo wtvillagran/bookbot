281c80384d8ac6efc63fcb39e0e78fb1d94c1767
William Villagran <wvillagran03@gmail.com>
2025-12-29 22:16:07 -0600

    report

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-from stats import get_count_words, get_count_char
+import sys
+from stats import get_count_words, get_count_char, print_report
 
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
-
-
+    
 
 def main():
-    path_to_book = "/Users/williamvillagran/Desktop/projects/Bookbot/books/frankenstein.txt"
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path_to_book = sys.argv[1]
     file_book = get_book_text(path_to_book)
-
     num_words = get_count_words(file_book)
-
-    print(f"Found {num_words} total words")
-
     num_chars = get_count_char(file_book)
-    
-    print(num_chars)
+
+    print_report(path_to_book, num_words, num_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -12,6 +12,14 @@ def get_count_char(file):
                 count_char[char_lower] = 1
             else:
                 count_char[char_lower] += 1
-
-        
     return count_char
+
+print_dict = lambda d: [print(f"{item[0]}: {item[1]}") for item in sorted(d.items(), key=lambda x: x[1], reverse=True) if ord(item[0]) < 128]
+
+def print_report(file_path, word_count, char_count):
+    print("============ BOOKBOT ============\n" \
+    f"Analyzing book found at {file_path}\n" \
+    "----------- Word Count ----------\n" \
+    f"Found {word_count} total words\n" \
+    f"--------- Character Count -------\n")
+    print_dict(char_count)
